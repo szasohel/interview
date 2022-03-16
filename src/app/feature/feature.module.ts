da1f71a4e85458d97853e6a598f96d5abe76ffa4
sayed sohel <sayed.x.sohel@questdiagnostics.com>
2022-03-16 14:38:30 -0400

    remove material

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './form/form.component';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field'
-import {MatIconModule} from '@angular/material/icon';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
@@ -14,11 +11,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    MatInputModule,
     ReactiveFormsModule,
     FormsModule,
-    MatIconModule,
-    MatFormFieldModule
   ],
   exports:[
     FormComponent
